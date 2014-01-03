3ec1eed4f90026d9ad4b3331ee5d4331cc2a431d
Anton Katsarov <anton@katsarov.org>
2014-01-03 15:55:22 +0200

    * Updated instalation scripts

--- uninstall.sh
+++ uninstall.sh
@@ -12,4 +12,41 @@ fi
 rm -rf /usr/local/cpanel/whostmgr/docroot/cgi/addon_cgpnewsletter*
 rm -f /usr/local/cpanel/whostmgr/docroot/templates/addon_cgpnewsletter_*
 
+# cPanel
 
+# Install cPanel CommuniGate Custom Module
+rm -f /usr/local/cpanel/Cpanel/CGPNewsletter.pm
+
+# cPanel Wrapper
+rm -rf /usr/local/cpanel/bin/admin/CGPNewsletter/
+
+# addon features
+rm -f /usr/local/cpanel/whostmgr/addonfeatures/cgpnewsletter
+
+
+# Install CommuniGate Plugin
+BASEDIR='/usr/local/cpanel/base/frontend';
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+THEMES=($(find ${BASEDIR} -maxdepth 1 -mindepth 1 -type d))
+IFS=$OLDIFS
+
+tLen=${#THEMES[@]}
+
+LOCALES=($(find ${PACKSRC}/locale -maxdepth 1 -mindepth 1))
+lLen=${#LOCALES[@]}
+
+for (( i=0; i<${tLen}; i++ ));
+do
+    rm -rf "${THEMES[$i]}/cgpnewsletter"
+    rm -f ${THEMES[$i]}/branding/cgpnewsletter_*
+    rm -f "${THEMES[$i]}/dynamicui/dynamicui_cgpnewletter.conf"
+
+    for ((j=0; j<${lLen}; j++)); do
+        TARGET=${THEMES[$i]}/locale/`basename ${LOCALES[$j]} '{}'`.yaml.local
+        sed -i -e '/^CGN/d' ${TARGET}
+    done
+done
+
+/usr/local/cpanel/bin/rebuild_sprites
+/usr/local/cpanel/bin/build_locale_databases
--- upgrade.sh
+++ upgrade.sh
@@ -17,3 +17,55 @@ then
     chmod +x ${PACKSRC}/scripts/register_apps.sh
     ${PACKSRC}/scripts/register_apps.sh
 fi
+
+# Check the scripts have executable flag
+chmod +x /usr/local/cpanel/whostmgr/docroot/cgi/addon_cgpnewsletter*
+
+# cPanel
+
+# Install cPanel CommuniGate Custom Module
+cp ${PACKSRC}/module/CGPNewsletter.pm /usr/local/cpanel/Cpanel/
+
+# cPanel Wrapper
+rm -rf /usr/local/cpanel/bin/admin/CGPNewsletter/
+cp -r ${PACKSRC}/admin/CGPNewsletter/ /usr/local/cpanel/bin/admin/
+chmod +x /usr/local/cpanel/bin/admin/CGPNewsletter/cca
+
+# addon features
+cp ${PACKSRC}/featurelists/cgpnewsletter /usr/local/cpanel/whostmgr/addonfeatures/
+
+
+# Install CommuniGate Plugin
+BASEDIR='/usr/local/cpanel/base/frontend';
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+THEMES=($(find ${BASEDIR} -maxdepth 1 -mindepth 1 -type d))
+IFS=$OLDIFS
+
+tLen=${#THEMES[@]}
+
+LOCALES=($(find ${PACKSRC}/locale -maxdepth 1 -mindepth 1))
+lLen=${#LOCALES[@]}
+
+for (( i=0; i<${tLen}; i++ ));
+do
+    rm -rf "${THEMES[$i]}/cgpnewsletter"
+    cp -r "${PACKSRC}/theme/cgpnewsletter" "${THEMES[$i]}/"
+    rm -f ${THEMES[$i]}/branding/cgpnewsletter_*
+    cp "${PACKSRC}/icons/"* "${THEMES[$i]}/branding"
+    cp "${PACKSRC}/plugin/dynamicui_cgpnewletter.conf" "${THEMES[$i]}/dynamicui/"
+
+    for ((j=0; j<${lLen}; j++)); do
+        TARGET=${THEMES[$i]}/locale/`basename ${LOCALES[$j]} '{}'`.yaml.local
+        if [ ! -f ${TARGET} ]
+        then
+            echo "---" > ${TARGET}
+        else
+            sed -i -e '/^CGN/d' ${TARGET}
+        fi
+        cat ${LOCALES[$j]} >> ${TARGET}
+    done
+done
+
+/usr/local/cpanel/bin/rebuild_sprites
+/usr/local/cpanel/bin/build_locale_databases
